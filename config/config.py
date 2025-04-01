bd81222741bb2ce9a884d9da1c54e937d9466ce3
Zeaisoft <zeaisoft.pvt.ltd@gmail.com>
2025-04-01 23:10:31 +0530

    Add files via upload

--- config/config.py
+++ config/config.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 # Load environment variables from .env
 load_dotenv()
 
-TWILIO_SID = os.getenv("TWILIO_SID")
-TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
-TWILIO_WHATSAPP_NUMBER = os.getenv("TWILIO_WHATSAPP_NUMBER")
+TWILIO_SID ="AC89a09a594306f1c30885a556bd0423d3"
+TWILIO_AUTH_TOKEN ="de1a5563fbfebcb019d9fe462b4c4b49"
+TWILIO_WHATSAPP_NUMBER = "+17074152982"
