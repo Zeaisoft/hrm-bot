01cc36d494b69122aa048671e0763a92589296c2
Zeaisoft <zeaisoft.pvt.ltd@gmail.com>
2025-04-01 23:51:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
-from flask import Flask
+from app import create_app
 
-def create_app():
-    app = Flask(__name__)
-    
-    # Any app configurations or routes
-    @app.route('/')
-    def hello_world():
-        return 'Hello, World!'
-    
-    return app
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
